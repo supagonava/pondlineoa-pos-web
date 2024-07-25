3e5454678a2cec69df761755a38235f2e19a6994
supakorn.emch <supakorn.emch@gmail.com>
2024-07-25 13:25:10 +0700

    chore: Disable export build

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 "use client";
 
 export default function Home() {
-    return <>ออกจากระบบแล้ว</>;
+    return (
+        <div className="flex flex-col items-center justify-center gap-1">
+            ออกจากระบบแล้ว{" "}
+            <a className="text-blue-400 underline" href="/login">
+                เข้าสู่ระบบ
+            </a>
+        </div>
+    );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    // distDir: 'dist',
-    // images: {
-    //     unoptimized: true,
-    // },
+    // output: 'export',
+    // exportTrailingSlash: true,
+    // distDir: 'dist'
 };
 
 export default nextConfig;
